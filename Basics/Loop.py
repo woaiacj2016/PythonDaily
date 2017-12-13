c0c33873caf5a8462604a1fb956b29e3565207f1
zz5724 <zz5724@gmail.com>
2017-12-13 20:51:27 +0800

    循环

---
+++ Basics/Loop.py
@@ -0,0 +1,21 @@
+'''
+循环
+'''
+
+#for...in循环
+
+num = ['one','two','three']
+
+for n in num:
+    print(n)
+
+#range函数-生成证书序列
+
+for n in range(5):
+    print(n)
+
+#while循环
+n = 0
+while n in range(5):
+    print(n)
+    n = n+1
